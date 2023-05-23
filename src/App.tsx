756fa0ea3e48c32ef3b1150f1f2c3823c3213e31
Dylan <zhaolandelong@163.com>
2023-05-24 01:57:28 +0800

    feat: add formlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import './App.css';
 import moment from 'moment';
-import { Checkbox, Button } from 'antd';
+import { Checkbox, Button, Layout, Space } from 'antd';
 import { fetchETFPrice, getOptionDealDate } from './utils';
 import type { ETFPriceInfo } from './types';
 import { ETF_INFOS, etfPosInfos } from './constants';
@@ -9,6 +9,9 @@ import ETFTable from './ETFTable';
 import ETFOpTable from './ETFOpTable';
 import PositionTable from './PositionTable';
 import InvestTable from './InvestTable';
+import PositionFormList from './PositionFormList';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 const App: React.FC = () => {
   const defaultCodes = etfPosInfos.map((info) => info.code);
@@ -41,29 +44,34 @@ const App: React.FC = () => {
   }, [fetchTime]);
 
   return (
-    <div className="App">
-      <h1>Today is {moment().format('YYYY-MM-DD dddd')}</h1>
-      <h2>
-        Nearest Deal Date: {optionDealDate.format('YYYY-MM-DD')}. Remain{' '}
-        <span className="red">{optionDealDate.diff(moment(), 'days') + 1}</span>{' '}
-        Days.
-      </h2>
-      <Checkbox.Group
-        options={checkboxOptions}
-        value={etfCodes}
-        onChange={(vals) => setEtfCodes(vals as string[])}
-      />
-      <Button
-        type="primary"
-        onClick={() => setfetchTime(moment().format('HH:mm:ss'))}
-      >
-        REFRESH
-      </Button>
-      <ETFTable dataSource={etfDataSource} fetchTime={fetchTime} />
-      <ETFOpTable etfPriceInfos={etfDataSource} fetchTime={fetchTime} />
-      <PositionTable etfPosInfos={etfPosInfos} />
-      <InvestTable etfPriceInfos={etfDataSource} etfPosInfos={etfPosInfos} />
-    </div>
+    <Layout>
+      <Content>
+        <h1>Today is {moment().format('YYYY-MM-DD dddd')}</h1>
+        <h2>
+          Nearest Deal Date: {optionDealDate.format('YYYY-MM-DD')}. Remain{' '}
+          <span className="red">
+            {optionDealDate.diff(moment(), 'days') + 1}
+          </span>{' '}
+          Days.
+        </h2>
+        <Checkbox.Group
+          options={checkboxOptions}
+          value={etfCodes}
+          onChange={(vals) => setEtfCodes(vals as string[])}
+        />
+        <Button
+          type="primary"
+          onClick={() => setfetchTime(moment().format('HH:mm:ss'))}
+        >
+          REFRESH
+        </Button>
+        {/* <PositionFormList /> */}
+        <ETFTable dataSource={etfDataSource} fetchTime={fetchTime} />
+        <ETFOpTable etfPriceInfos={etfDataSource} fetchTime={fetchTime} />
+        <PositionTable etfPosInfos={etfPosInfos} />
+        <InvestTable etfPriceInfos={etfDataSource} etfPosInfos={etfPosInfos} />
+      </Content>
+    </Layout>
   );
 };
 
---
+++ src/PositionFormList.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, Form, Space, Select, InputNumber, Input } from 'antd';
+import { etfPosInfos, ETF_INFOS } from './constants';
+
+const onFinish = (values: any) => {
+  console.log('Received values of form:', values);
+};
+
+const PositionFormList: React.FC = () => (
+  <Form
+    name="dynamic_form_nest_item"
+    onFinish={onFinish}
+    style={{ maxWidth: 600 }}
+    autoComplete="off"
+    initialValues={{ posInfos: etfPosInfos }}
+  >
+    <Form.List name="posInfos">
+      {(fields, { add, remove }) => (
+        <>
+          {fields.map(({ key, name, ...restField }) => (
+            <Space
+              key={key}
+              style={{ display: 'flex', marginBottom: 8 }}
+              align="baseline"
+            >
+              <Form.Item
+                {...restField}
+                name={[name, 'code']}
+                rules={[{ required: true, message: 'Missing first name' }]}
+              >
+                <Select
+                  placeholder="code"
+                  options={Object.values(ETF_INFOS).map((info) => ({
+                    label: info.name,
+                    value: info.code,
+                  }))}
+                />
+              </Form.Item>
+              <Form.Item
+                {...restField}
+                name={[name, 'startDate']}
+                rules={[{ required: true, message: 'Missing Start Date' }]}
+              >
+                <Input placeholder="Start Date" />
+              </Form.Item>
+              <Form.Item
+                {...restField}
+                name={[name, 'monthlyAmount']}
+                rules={[{ required: true, message: 'Missing Monthly Amount' }]}
+              >
+                <InputNumber placeholder="Monthly Amount" />
+              </Form.Item>
+              <Form.Item
+                {...restField}
+                name={[name, 'expectedReturnRate']}
+                rules={[
+                  { required: true, message: 'Missing Expected Return Rate' },
+                ]}
+              >
+                <InputNumber placeholder="Expected Return Rate" />
+              </Form.Item>
+              <Form.Item
+                {...restField}
+                name={[name, 'scalingMutiple']}
+                rules={[{ required: true, message: 'Missing Scaling Mutiple' }]}
+              >
+                <InputNumber placeholder="Scaling Mutiple" />
+              </Form.Item>
+              <MinusCircleOutlined onClick={() => remove(name)} />
+            </Space>
+          ))}
+          <Form.Item>
+            <Button
+              type="dashed"
+              onClick={() => add()}
+              block
+              icon={<PlusOutlined />}
+            >
+              Add field
+            </Button>
+          </Form.Item>
+        </>
+      )}
+    </Form.List>
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+);
+
+export default PositionFormList;
