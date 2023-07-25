fe976fdb3b4b7138a87c6c368f410261fcce24e0
Dylan <zhaolandelong@163.com>
2023-07-25 20:55:13 +0800

    feat: add instructions

--- src/components/PayArticle/index.tsx
+++ src/components/PayArticle/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Divider, Typography } from 'antd';
-import ImgPayArticle from './pay-article.png';
+import { Divider, Typography, QRCode } from 'antd';
+import img from './instructions.png';
+// import icon from './icon.svg';
+
 const { Paragraph, Text } = Typography;
 
 const QR: React.FC = () => (
   <>
-    <Divider>请我喝杯🥤？</Divider>
+    <Divider>使用说明</Divider>
     <div style={{ textAlign: 'center' }}>
-      <Paragraph>如果这个🔧帮你赚（省）💰了，就请我喝杯🥤吧~</Paragraph>
+      <Paragraph>如果看了备注还不会用，看看这个说明书吧。</Paragraph>
       {/* <QRCode
-        value="https://mp.weixin.qq.com/s/P5yCaZaWzOMAruYpkxkjlw"
+        value="https://mp.weixin.qq.com/s/gNQeioR7n70sxON8N1W2rg"
         errorLevel="H"
         icon={icon}
       /> */}
-      <img style={{ width: 100 }} src={ImgPayArticle} alt="pay article" />
+      <img style={{ width: 100 }} src={img} alt="pay article" />
       <br />
       <Text type="secondary">长按识别二维码</Text>
     </div>
-    <Divider>有彩蛋哦~祝大家早日💰自由</Divider>
+    <Divider>看懂了的话记得请我喝杯🥤哦~</Divider>
   </>
 );
 
